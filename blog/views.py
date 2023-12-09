e41c6c3643d74cea6739f5d44392757cc7515f15
Radmuire <asainovrad@yandex.ru>
2023-12-09 15:22:24 +0300

    Adding translator

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.views.generic import ListView, TemplateView
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import render
 
 class BlogListView(ListView):
+    paginate_by = 3
     model = Post
     template_name = 'home.html'
 
